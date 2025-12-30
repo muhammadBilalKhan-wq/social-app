1f3d6cdbcbb042b5dfcc562736c80db5eee0342c
theelonberg <theelonberg@gmail.com>
2025-12-30 21:38:41 +0000

    First commit from fstudio

--- checking_sn/app/src/main/java/com/socialnetwork/checking_sn/ui/components/StandardScaffold.kt
+++ checking_sn/app/src/main/java/com/socialnetwork/checking_sn/ui/components/StandardScaffold.kt
@@ -1,14 +1,14 @@
-
 package com.socialnetwork.checking_sn.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -16,19 +16,11 @@ fun StandardScaffold(
     navController: NavController,
     content: @Composable () -> Unit
 ) {
-    val scaffoldState = rememberScaffoldState()
-
     Scaffold(
-        scaffoldState = scaffoldState,
         floatingActionButton = {
-            Box(
-                modifier = Modifier
-                    .padding(bottom = 60.dp), 
-                contentAlignment = Alignment.BottomCenter
-            ) {
-                FloatingAddButton(navController = navController)
-            }
+            FloatingAddButton(navController = navController)
         },
+        floatingActionButtonPosition = FabPosition.Center,
         bottomBar = {
             BottomNavBar(navController = navController)
         },
